acd7fa414d2a99e22a99e4f6a1c01d4596982994
Ayala-crea <ayalarifki@gmail.com>
2024-06-11 23:18:53 +0700

    cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connectDB();
 
 app.use(
     cors({
-      origin: ["http://localhost:5000"],
+      origin: ["http://localhost:5173"],
       methods: ["GET", "POST", "PUT", "OPTIONS", "DELETE"],
       credentials: true,
       optionsSuccessStatus: 200,
