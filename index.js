ee91796cb224e1ed9c99a64e618ad64acd2b84cb
Ayala-crea <ayalarifki@gmail.com>
2024-06-18 14:17:41 +0700

    method payments

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const authRoutes = require('./routes/authRoutes.js');
 const productRoutes = require('./routes/productRoutes.js');
 const cartRoutes = require('./routes/cartRoutes.js');
 const imageRoutes = require('./routes/imageRoutes.js');
+const metodRoutes = require('./routes/metodRoutes.js');
+
+const MetodePembayaran = require('./models/metode_pembayaran.js'); 
 
 const app = express();
 app.use(express.json());
@@ -31,9 +34,12 @@ app.use('/api/product', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use(express.static('img'));
 app.use('/', imageRoutes);
+app.use('/api/payments', metodRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
+
 });
--- models/metode_pembayaran.js
+++ models/metode_pembayaran.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const metodePembayaranSchema = new Schema({
-    id_metode_pembayaran: { type: Number, required: true, unique: true },
     nama_metode: { type: String, required: true },
     deskripsi_metode_pembayaran: { type: String, required: true }
 });
 
-const MetodePembayaran = mongoose.model('MetodePembayaran', metodePembayaranSchema);
+const collectionName = 'MetodePembayaran';
+const MetodePembayaran = mongoose.model('MetodePembayaran', metodePembayaranSchema, collectionName);
 module.exports = MetodePembayaran;
